6f8a96706374ccccf7bfae1370c173766a441f03
ScorpiaWeb <info@team-scorpia.de>
2017-01-30 19:08:54 +0100

    Fixed smaller bug with a-tags

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+$(function(){
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        &amp;amp;&amp;amp; location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length &amp;amp;&amp;amp; $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
 });
