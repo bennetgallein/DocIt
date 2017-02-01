1f65e0370f65d595a90dd906cb26092ecb13e246
ScorpiaWeb <info@team-scorpia.de>
2017-02-01 15:35:20 +0100

    see todo.txt for update infos

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 DocIt is a free to use HTML Website Template for Developers to documentate their APIs or anything else.
 
-DocIt is using, jQuery, Morris.js, Prettify.
+DocIt is using, jQuery, Morris.js, Prettify and the Go.gl API.
 
 
 Please post suggestions under the "Issues" Tab.
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -12,3 +12,20 @@ $(function() {
     }
   });
 });
+$(document).ready(function() {
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});
+
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
